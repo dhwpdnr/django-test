7c62c054f0d336f8c4bbc23b865c091a84ad09fe
dhwpdnr <ink1237@gmail.com>
2022-09-30 19:13:29 +0900

    css

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .apis import TodoCreateAPI, TodoDetailAPI, TodoUpdateAPI, TodoDeleteAPI
+from .views import TodoCreateView
 
 urlpatterns = [
     path('api/create/',TodoCreateAPI.as_view()),
     path('api/detail/<int:pk>',TodoDetailAPI.as_view()),
     path('api/update/<int:pk>',TodoUpdateAPI.as_view()),
-    path('api/delete/<int:pk>',TodoDeleteAPI.as_view())
+    path('api/delete/<int:pk>',TodoDeleteAPI.as_view()),
+
+    path('create/', TodoCreateView.as_view())
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class TodoCreateView(APIView):
+    def get(self, request):
+        return render(request, "todo/create.html",{})
